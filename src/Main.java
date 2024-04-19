a58139a615e3bb0c776f3dc9b2b4f7441427e5c7
ehsansdr <sehsan251@gmail.com>
2024-04-19 16:54:52 +0330

    14. Longest Common Prefix

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,28 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        //https://leetcode.com/problems/palindrome-number/description/
+        // https://leetcode.com/problems/longest-common-prefix/
 
-        System.out.println(romanToInt("CIV"));
+        String[] strs = new  String[]{"flower","flow","flight"};
+
+        System.out.println(longestCommonPrefix(strs));
     }
 
-    public static int romanToInt(String s) {
-        Map<Character, Integer> m = new HashMap<>();
+    public static String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);              // this help us to have better solution idea
+        String element1 = strs[0];      // becuase it is sorted we get first element
+        String element2 = strs[strs.length-1];    // and last element
+        int length = 0;                 //we use this becuase it is sorted
 
-        m.put('I', 1);
-        m.put('V', 5);
-        m.put('X', 10);
-        m.put('L', 50);
-        m.put('C', 100);
-        m.put('D', 500);
-        m.put('M', 1000);
-
-        // in "IV" case becuase I is lower than the V and V is next of that
-        // so "I" turn to negative becuase of upper role and "I" is equal -1 ,not 1 and "V" is 5 so we sum them and get 4
-        // in loop we obey this role in kind of
-        // in CIV ,we do not minus "V" of "I" we minus that from "C"
-        // "C" = 100 , "I" = -1  becuse the next letter is greater it turns to negative
-        // "V" = 5 and sum of them is 100 + (-1) + 5 = 104
-
-
-        int ans = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (i < s.length() - 1 &&
-                    m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
-                ans -= m.get(s.charAt(i));
+        while(length < element1.length() && length < element2.length()){
+            if(element1.charAt(length) == element2.charAt(length)){
+                length++;
             } else {
-                ans += m.get(s.charAt(i));
+                break;
             }
         }
 
-        return ans;    }
-
-
+        return element1.substring(0, length);
+    }
 }
